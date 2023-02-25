50eb2c412274df2f6a135a6df2362533032dbd9e
Logan Hansen <logan.moon.hansen@gmail.com>
2023-02-25 13:04:47 -0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+
+
 /* Open when someone clicks on the span element */
 function openNav() {
+  const body = document.querySelector("body");
   document.getElementById("overlayNav").style.width = "100%";
+  // Disable scroll
+  body.style.overflow = "hidden";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
+  const body = document.querySelector("body");
   document.getElementById("overlayNav").style.width = "0%";
+  // Enable scroll
+  body.style.overflow = "auto";
 }
